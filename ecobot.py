d7f0b23cabd0f556ad67648da59368c6b4c2b959
Dan0n4ik11 <160390137+Dan0n4ik11@users.noreply.github.com>
2024-03-10 11:17:47 +0300

    Update ecobot.py

--- ecobot.py
+++ ecobot.py
@@ -1,13 +1,64 @@
 import discord
 from discord.ext import commands
+import requests
+import random
 
 bot = commands.Bot(command_prefix='+', intents=discord.Intents().all())
 
 
-@bot.event
-async def on_ready(ctx):
-    print(f'logged {bot.user}')
-    await ctx.send(f'ПРИВЕТ {bot.user}!')
+@bot.command('rmem')
+async def randommem(ctx):
+    a = ['project/mem1.jpg', 'project/mem2.jpg', 'project/mem3.jpg']
+    b = random.randint(0, 2)
+    with open(a[b], 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+
+@bot.command('rduck')
+async def duck(ctx):
+    def get_duck_image_url():
+        url = 'https://random-d.uk/api/random'
+        res = requests.get(url)
+        data = res.json()
+        return data['url']
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+@bot.command('rmot')
+async def cute(ctx):
+    a = ['Ты лучший', 'У тебя всё получится', 'Удачи тебе']
+    b = random.randint(0, 2)
+    await ctx.reply(a[b])
+    c = ['project/cat1.jpg', 'project/cat2.jpg', 'project/cat3.jpg',
+         'project/cat4.jpg', 'project/cat5.jpg',]
+    d = random.randint(0, 4)
+    with open(c[d], 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+
+@bot.command('yt')
+async def yt(ctx):
+    await ctx.reply('''Ссылка на канал гринпис https://www.youtube.com/@greenpeace
+Ссылка на канал WWF https://www.youtube.com/@WWFunitedkingdom''')
+
+
+@bot.command('h')
+async def help(ctx):
+    await ctx.reply('''Список в данный момент существующих команд:
+yt
+plastic
+metal
+dirt
+wood
+clay
+stone
+glass
+concrete''')
+
+# alexander part
 
 
 @bot.command('plastic')
@@ -50,15 +101,4 @@ async def concrete(ctx):
     await ctx.send(f'50%')
 
 
-@bot.command('ecohelp')
-async def help(ctx):
-    await ctx.reply('''+yt - ссылки на ютуб каналы эко-организаций''')
-
-
-@bot.command('yt')
-async def yt(ctx):
-    print('''Ссылка на канал гринпис https://www.youtube.com/@greenpeace
-          Ссылка на канал WWF https://www.youtube.com/@WWFunitedkingdom''')
-
-
-bot.run('MTIxNjE1OTkzODg0MjIwMjE0Mw.GqK48H.6GN9d6r31F7hAOuEuKruyLKNmzgdLz3T_fejX8')
+bot.run('TOKEN')
